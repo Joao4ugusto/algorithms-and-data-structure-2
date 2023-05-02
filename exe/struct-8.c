856ac6b0cc59fdde46cedc8728fbe53c9fa1263b
João <joaoaugusto311016@gmail.com>
2023-05-02 17:57:02 -0300

    02-05

--- exe/struct-8.c
+++ exe/struct-8.c
@@ -1,33 +1,37 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
-typedef struct _livro{
-    char titulo[100];
-    char autor[100];
-    int ano;
-} Livro;
+struct Livro {
+    char titulo[50];
+    char autor[50];
+    int ano_publicacao;
+};
 
+int main() {
+    struct Livro livros[5];
+    int ano_pesquisa;
 
-void add_livro(Livro *l, char *titulo, char *autor, int ano){
-    strcpy(l->titulo, titulo);
-    strcpy(l->autor, autor);
-    l->ano = ano;
-}
+    // Preenchendo informações dos livros
+    for (int i = 0; i < 5; i++) {
+        printf("Digite as informações do livro %d:\n", i + 1);
+        printf("Título: ");
+        scanf("%s", livros[i].titulo);
+        printf("Autor: ");
+        scanf("%s", livros[i].autor);
+        printf("Ano de publicação: ");
+        scanf("%d", &livros[i].ano_publicacao);
+    }
 
-void printf_livro(const Livro *l){
-    printf("Titulo = %s\n", l->titulo);
-    printf("Autor = %s\n", l->autor);
-    printf("Ano = %d\n", l->ano);
-}
+    // Pedindo ano de pesquisa
+    printf("\nDigite um ano para pesquisar: ");
+    scanf("%d", &ano_pesquisa);
 
-int main(){
-
-
-   Livro livro[3];
-   
-   add_livro(&livro[0], "Girua", "Joao Augusto", 2002);
-   printf_livro(&livro);
+    // Imprimindo títulos dos livros publicados no ano de pesquisa
+    printf("\nLivros publicados em %d:\n", ano_pesquisa);
+    for (int i = 0; i < 5; i++) {
+        if (livros[i].ano_publicacao == ano_pesquisa) {
+            printf("- %s\n", livros[i].titulo);
+        }
+    }
 
     return 0;
 }
